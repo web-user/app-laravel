32ed66350d627c7486a2ec83293ff6199f12f54c
Volodymyr Pivnychenko <pivnychenko@Volodymyrs-MacBook-Pro.local>
2022-11-17 17:06:25 -0800

    add filter

---
+++ app/Services/V1/CustomerQuery.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services\V1;
+
+use Illuminate\Http\Request;
+
+
+class CustomerQuery {
+    protected $safeParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'qt', 'lt']
+    ];
+
+
+
+    protected $columnMap = [
+        'postalCode' => 'postal_code'
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '≤',
+        'gt' => '>',
+        'gte' => '≥'
+    ];
+
+    public function transform(Request $requesr) {
+        $eloQuery = [];
+
+        foreach ($this->safeParams as $param => $operators ) {
+            $query = $requesr->query($param);
+
+            if (!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+
+        }
+
+        return $eloQuery;
+    }
+}
---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer; 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Customer::factory()->count(25)->hasInvoices(10)->create();
+
+        Customer::factory()->count(50)->hasInvoices(3)->create();
+
+        Customer::factory()->count(5)->create();
+    }
+}
